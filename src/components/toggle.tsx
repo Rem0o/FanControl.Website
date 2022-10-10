6d9d6f1ac431d2f3a2cf73edc786dd4d70de3dd9
Rem0o <rem.merc@outlook.com>
2022-10-10 17:53:59 -0400

    Update toggle.tsx

--- src/components/toggle.tsx
+++ src/components/toggle.tsx
@@ -1,33 +1,32 @@
-import * as React from "react"
+import * as React from "react";
 
 type ToggleProps = {
-    checkedState: [boolean, React.Dispatch<React.SetStateAction<boolean>>]
-    text?: string,
-}
+  checkedState: [boolean, React.Dispatch<React.SetStateAction<boolean>>];
+  text?: string;
+};
 
 export default function Toggle({ checkedState, text }: ToggleProps) {
+  const [checked, setChecked] = checkedState;
 
-    const [checked, setChecked] = checkedState;
+  const onClick = () => {
+    setChecked(!checked);
+  };
 
-    const onClick = () => {
-        setChecked(!checked);
-    };
-
-    return (
-        <label className="inline-flex relative cursor-pointer self-center ">
-            <input
-                type="checkbox"
-                className="sr-only peer"
-                checked={checked}
-                readOnly
-            />
-            <div
-                onClick={onClick}
-                className="w-11 h-6 bg-gray-200 rounded-full peer  peer-focus:ring-blue-300  peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-slate-500"
-            ></div>
-            <span onClick={onClick} className="ml-2 text-sm font-medium select-none">
-                {text}
-            </span>
-        </label>
-    );
+  return (
+    <label className="mr-1 inline-flex py-1 relative cursor-pointer self-center">
+      <input
+        type="checkbox"
+        className="sr-only peer"
+        checked={checked}
+        readOnly
+      />
+      <div
+        onClick={onClick}
+        className="w-9 h-4 bg-gray-200 rounded-full peer peer-focus:ring-blue-300 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-slate-500"
+      ></div>
+      <span onClick={onClick} className="ml-2 text-sm font-medium select-none">
+        {text}
+      </span>
+    </label>
+  );
 }
