7214d2b454550a8143bbcfbe03a6c3fa558eeb4d
Rem0o <remi.mercier.software@gmail.com>
2024-10-24 16:56:50 -0400

    Fix grammar of SEO description.

--- src/common/seo.ts
+++ src/common/seo.ts
@@ -1,6 +1,6 @@
 export default {
   title: `Fan Control`,
-  description: `Fan Control is a free software that allows the user to control his CPU, GPU and case fans using temperatures.`,
+  description: `Fan Control is a free software that allows the user to control their CPU, GPU and case fans using temperatures.`,
   siteUrl: `https://getfancontrol.com/`,
   twitterUsername: "",
   image: ""
